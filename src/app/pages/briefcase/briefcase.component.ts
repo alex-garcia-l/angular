50bd53d84f8087cc9d57ffd34a1272e53ea7d9b5
Eduardo Alejandro García López <a.garcia@quadsys.mx>
2020-01-15 16:48:18 -0600

    Templete listo sin rutas

---
+++ src/app/pages/briefcase/briefcase.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-briefcase',
+  templateUrl: './briefcase.component.html',
+  styleUrls: ['./briefcase.component.sass']
+})
+export class BriefcaseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
